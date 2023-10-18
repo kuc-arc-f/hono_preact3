0923dab57ac60024aa385b73cb649757cf371a2f
_ñ <knaka0209@gmail.com>
2023-10-19 08:00:14 +0900

    up, 1019a2

--- public/js/preact2/index.js
+++ public/js/preact2/index.js
@@ -71,6 +71,52 @@ const PageIndex = {
      * @param
      *
      * @return
+     */  
+    addItem : async function()
+    {
+        try{
+            let ret = false;
+            let titleValue = "";
+            const title = document.querySelector("#title");
+            if(title) {
+                titleValue = title.value;
+            }
+            const item = {
+                title: titleValue,
+                content: "",
+            }
+console.log(item);
+//return;
+console.log("title=", titleValue);
+            const body = JSON.stringify(item);		
+            const res = await fetch("/api/tasks/create", {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},      
+                body: body
+            });
+            const json = await res.json()
+console.log(json);   
+            if (res.status !== 200) {
+                console.error("error, status <> 200");
+                throw new Error(await res.text());
+            }
+            if (json.ret !==  "OK") {
+                console.error("Error, json.ret <> OK");
+                return ret;
+            }
+            ret = true;
+            return ret;
+        } catch (e) {
+            console.error("Error, addItem");
+            console.error(e);
+            throw new Error('Error , addItem');
+        }
+    },     
+    /**
+     *
+     * @param
+     *
+     * @return
      */     
     initProc: async function() {
         try {
@@ -78,6 +124,15 @@ console.log("#init=", new Date().toString());
             const items = await this.get_list();
 console.log(items);
             this.displayProc(items);
+            //
+            const button = document.querySelector('#save');
+            button.addEventListener('click', async () => {
+                const result = await this.addItem();
+console.log("result=", result);
+                if(result === true) {
+                    location.reload();
+                }
+            });            
         } catch (e) {
             console.error("Error, get_list");
             console.error(e);
--- src/views/preact2/App.tsx
+++ src/views/preact2/App.tsx
@@ -10,6 +10,13 @@ export const Test5: FC<{ items: any[] }> = (props: { items: any[] }) => {
       <div>
         <h1 class="text-4xl font-bold">Welcome , Preact2 </h1>
         <hr class="my-2" />
+        <hr class="my-2" />
+            <label>Title:</label>
+            <input type="text" id="title" 
+            class="border border-gray-400 rounded-md px-3 py-2 w-full focus:outline-none focus:border-blue-500"/>
+        <hr class="mt-2 mb-1" />
+        <button id="save" class="btn-purple ms-2 my-2">Save</button>
+        <hr class="my-1" />
         <div id="root"></div>
       </div>
       {/* JS */}
